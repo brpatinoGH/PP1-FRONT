3451ad1da150588228f419bc0743c150a3087e58
Laila Vazquez <laila.vazquez.lv@gmail.com>
2024-09-17 20:57:49 -0300

    informacion y pedido

---
+++ PP1-NOGH/calendario/perfil/index.js
@@ -0,0 +1,20 @@
+document.getElementById('btn1').addEventListener('click', function(){
+    window.location.href = './informacion/informacion.html';
+});
+
+document.getElementById('btn2').addEventListener('click', function(){
+    window.location.href = './';
+});
+
+document.getElementById('btn3').addEventListener('click', function(){
+    window.location.href = './';
+});
+
+document.getElementById('btn4').addEventListener('click', function(){
+    window.location.href = './';
+});
+
+
+document.getElementById('btn5').addEventListener('click', function(){
+    window.location.href = '../../../bienvenido/index.html';
+});
