2535cf949936b9825fcf480d3ccefaec5241696a
Laila Vazquez <laila.vazquez.lv@gmail.com>
2024-09-22 00:27:10 -0300

    perfillll

--- PP1-NOGH/calendario/perfil/index.js
+++ PP1-NOGH/calendario/perfil/index.js
@@ -1,3 +1,7 @@
+document.getElementById('btn1').addEventListener('click', function(){
+    window.location.href = './';
+});
+
 document.getElementById('btn2').addEventListener('click', function(){
     window.location.href = './configuracion/configuracion.html';
 });
@@ -6,15 +10,15 @@ document.getElementById('btn3').addEventListener('click', function(){
     window.location.href = './pedidos/pedidos.html';
 });
 
-document.getElementById('btn3').addEventListener('click', function(){
-    window.location.href = './';
-});
-
 document.getElementById('btn4').addEventListener('click', function(){
     window.location.href = './';
 });
 
-
 document.getElementById('btn5').addEventListener('click', function(){
+    window.location.href = './';
+});
+
+
+document.getElementById('btn6').addEventListener('click', function(){
     window.location.href = '../../../bienvenido/index.html';
 });
