86b7581cfd989c24de710c0fc91443e8e40e8236
Laila Vazquez <laila.vazquez.lv@gmail.com>
2024-10-15 19:42:46 -0300

    ss

--- PP1-NOGH/bienvenido/index.js
+++ PP1-NOGH/bienvenido/index.js
@@ -1,14 +1,6 @@
 document.getElementById('btn inicio').addEventListener('click', function(){
     window.location.href = './inicia/inicia.html';
 });
-
-document.getElementById('btn registrarse').addEventListener('click', function(){
-    window.location.href = './registrarse/registrarse.html';
-});
-document.getElementById('btn inicio').addEventListener('click', function(){
-    window.location.href = './inicia/inicia.html';
-});
-
 document.getElementById('btn registrarse').addEventListener('click', function(){
     window.location.href = './registrarse/registrarse.html';
 });
